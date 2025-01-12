04996b0ca065be5511e74337281b5acfa9c6edd3
chadixearth <richard.legaspi.2003@outlook.com>
2025-01-12 20:33:44 +0800

    Fix: Update merge migrations to fix dependency chain

--- tailwind_django/purchasing/migrations/0024_merge_20250112_2033.py
+++ tailwind_django/purchasing/migrations/0024_merge_20250112_2033.py
@@ -2,8 +2,8 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('purchasing', '0023_merge_20250106_1354'),
-        ('purchasing', '0024_remove_delivery_warehouse'),
+        ('purchasing', '0023_merge_20250112_2026'),
+        ('purchasing', '0024_auto_20250107_2329'),
     ]
 
     operations = []
