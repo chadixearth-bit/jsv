44fc05a46c79d1e83ed2f59d9d6e196a555e8629
chadixearth <richard.legaspi.2003@outlook.com>
2025-01-12 20:36:33 +0800

    Fix: Update merge migration dependencies

--- tailwind_django/purchasing/migrations/0024_merge_20250112_2033.py
+++ tailwind_django/purchasing/migrations/0024_merge_20250112_2033.py
@@ -3,7 +3,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
     dependencies = [
         ('purchasing', '0023_merge_20250112_2026'),
-        ('purchasing', '0024_auto_20250107_2329'),
+        ('purchasing', '0024_remove_delivery_warehouse'),
     ]
 
     operations = []
