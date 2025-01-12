8078a607bcad5d241489da3b04f05de38d4eb91c
chadixearth <richard.legaspi.2003@outlook.com>
2025-01-12 15:50:46 +0800

    Update build.sh to explicitly install xhtml2pdf dependency

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 set -o errexit
 
 cd tailwind_django
+pip install --upgrade pip
 pip install -r ../requirements.txt
+pip install xhtml2pdf==0.2.13  # Explicitly install xhtml2pdf
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py create_default_superuser
