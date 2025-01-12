14f798b823bd16b88410059719d26ff21abe31c3
chadixearth <richard.legaspi.2003@outlook.com>
2025-01-12 20:26:29 +0800

    Fix: Clean up merge migrations for subtotal field

---
+++ tailwind_django/purchasing/migrations/0023_merge_20250112_2026.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('purchasing', '0019_fix_subtotal_field'),
+        ('purchasing', '0018_auto_20250104_1350'),
+    ]
+
+    operations = []
